0e469db9fb3edd42dc26842c916ea2d13c464031
0802222 <0802222@naver.com>
2024-11-03 21:15:26 +0900

    init: init new java spring project

---
+++ src/main/java/npsm/sandbox/matchnest/MatchNestApplication.java
@@ -0,0 +1,13 @@
+package npsm.sandbox.matchnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchNestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatchNestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/npsm/sandbox/matchnest/MatchNestApplicationTests.java
@@ -0,0 +1,13 @@
+package npsm.sandbox.matchnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
